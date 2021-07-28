0ac83fec56bbe4c0280d3428e876497dfdfcb06b
Gavin Acquroff <gavina352@gmail.com>
2021-07-28 21:45:24 +0000

    refactor stdin setup to input.js

--- play.js
+++ play.js
@@ -1,28 +1,9 @@
-const net = require("net");
 const connect = require('./client');
-//initialize functions before calling them
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  return stdin;
-};
+const setupInput = require('./input');
 
-const handleUserInput = data => {
-  const ctrlC = "\u0003";
-
-  if (data === ctrlC) {
-    process.exit();
-  }
-};
+// setup stdin
+const userInput = setupInput();
 
 // Connect to server
 console.log("Connecting ...");
 connect();
-
-// create stdin object
-const userInput = setupInput();
-
-userInput.on("data", handleUserInput);
