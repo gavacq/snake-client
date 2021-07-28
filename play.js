7b57057750b7807252e836f1d9bef60b80e1513a
Gavin Acquroff <gavina352@gmail.com>
2021-07-28 21:37:34 +0000

    add stdin to play.js

--- play.js
+++ play.js
@@ -1,4 +1,28 @@
-const connect = require("./client");
+const net = require("net");
+const connect = require('./client');
+//initialize functions before calling them
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  return stdin;
+};
 
+const handleUserInput = data => {
+  const ctrlC = "\u0003";
+
+  if (data === ctrlC) {
+    process.exit();
+  }
+};
+
+// Connect to server
 console.log("Connecting ...");
 connect();
+
+// create stdin object
+const userInput = setupInput();
+
+userInput.on("data", handleUserInput);
